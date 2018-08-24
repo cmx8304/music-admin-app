44c4bd14cb016350c79ce38b65d45cf25a771d35
cmx8304 <18761668304@163.com>
2018-08-24 17:28:39 +0800

    优化

--- src/Server.js
+++ src/Server.js
@@ -2,22 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const uuidv4 = require('uuid/v4');
 
-//require 语句，是 node.js 提供的 import 方式
+var multer = require('multer');
+var upload = multer({dest:'public/uploads'});
 const app = express();
-//用于解析 form 提交的内容，并且把 body 内容转换为 json 格式
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 //缓存歌单数据
-const playlistDatas = [
+const playlistDatas =[
     {
-        id:1,
-        name:'测试歌单1',
-        cover:'http://xx.com/xx.jpg',
-        desc:'测试歌单描述',
+        id: 1,
+        name: '测试歌单1',
+        cover: 'http://xx.com/xx.jpg',
+        desc: '测试歌单描述',
         musicKeys:['515143071']
     }
 ];
-
+//测试歌 123
 const musicDatas = [
     {
         id:'515143071',
@@ -37,7 +38,6 @@ const musicDatas = [
 
     }
 ];
-
 /**
  * 处理歌单添加
  */
@@ -49,22 +49,17 @@ app.post('/api/playlist/add',function(req,res){
     res.json(req.body);
 });
 
-/**
- * 处理歌单修改
- */
+//处理歌单添加 第六课
+
 app.post('/api/playlist/update',function(req,res){
     const id = req.body.id;
-
-    playlistDatas.map(item => {
+    playlistDatas.map(item =>{
         if(item.id === id){
             item.musicKeys = req.body.keys;
         }
     });
-    res.json({
-        success: 'success'
-    });
-})
-
+    res.json({success:'success'})
+});
 /**
  * 处理歌单列表数据
  */
@@ -72,27 +67,32 @@ app.get('/api/playlist/query',function(req,res){
     res.json(playlistDatas);
 });
 
-/**
- * 获取歌曲的数据
- */
+//获取歌曲的数据
 app.get('/api/music/query',function(req,res){
-    //请求参数是有传了ids参数
     if(req.query.ids){
-        //转换为数据便于比较
         const ids = JSON.parse(req.query.ids);
         const result = [];
-        ids.map(id => {
-            musicDatas.map(mid => {
-                //比较请求参数的id是否存在于musicdata里，有则存入临时数组
-                if(id === mid.id){
-                    result.push(mid);
-                }
-            });
+        ids.map(id =>{
+            musicDatas.map(mid=>{
+            if(id === mid.id){
+                result.push(mid);
+            }
         });
-        res.json(result);
-        return;
-    }
-    res.json(musicDatas);
+    });
+    res.json(result);
+    return;
+}
+ res.json(musicDatas);
+});
+//第九课添加的 upload 、post 方法
+app.post('/upload',upload.single('music'),function(req,res){
+    res.json(req.file);
+});
+app.post('/api/music/add',function(req,res){
+    req.body.id = uuidv4();
+    req.body.url = 'uploads/'+req.body.url;
+    musicDatas.push(req.body)
+    res.json(req.body);
 });
 
 app.listen(4000,function(){
--- src/page/Songs.js
+++ src/page/Songs.js
@@ -1,13 +1,107 @@
 import React, { Component } from 'react';
+import {Form, Input, Button, Modal, Upload, message, Icon} from 'antd';
+
+import Remote from '../Remote';
+
+const FormItem = Form.Item;
+const {TextArea} = Input;
+
+//这个props是用于upload上传组件的配置
+//music与Server.js里的upload.single('music')里的music一致
+const props = {
+    name:'music',
+    action:'/upload/',
+    onChange:info =>{
+        if(info.file.status !=='uploading'){
+            console.log(info.file,info.fileList);
+        }
+        if(info.file.status ==='done'){
+            console.log(info.file.response.path);
+            message.success(`${info.file.name}file uploaded successfully`);
+        }else if(info.file.status ==='error'){
+            message.error(`${info.file.name} file upload failed.`);
+        }    
+      }
+    };
+
 class Songs extends Component {
     componentWillMount() {
-        document.title = "歌曲管理"
+        document.title = "歌曲管理";
     }
+
+    handleSubmit = e =>{
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if(!err){
+                console.log('Received values of form:',  values);
+                const body = {
+                    title: values.title,
+                    url: values.file.file.response.filename
+                };
+                Remote(
+                    '/api/music/add',
+                    {
+                        body: JSON.stringify(body),
+                        method: 'POST'
+                    },
+                    data => {
+                        console.log(data);
+                    }
+                );
+
+            }
+        });
+    };
+
     render() {
+        const {getFieldDecorator} = this.props.form;
+        const formItemLayout = {
+            labelCol : {
+                xs: {span: 24},
+                sm: {span: 6}
+            },
+            wrapperCol: {
+                xs: {span: 24},
+                sm: {span: 12}
+            }
+        };
+        
+        
         return (
-            <div>Songs</div>
+            <Form onSubmit={this.handleSubmit}>
+                <FormItem label="歌曲名称" {...formItemLayout}>
+                    {getFieldDecorator('title',{
+                        rules: [
+                            {
+                                required: true,
+                                message: '请输入歌曲的名称！'
+                            }
+                        ]
+                    })(<Input placeholder="请输入歌曲的名称"/>)}
+                </FormItem>
+                <FormItem label="上传歌曲" {...formItemLayout}>
+                    {getFieldDecorator('file',{
+                        rules: [
+                            {
+                                required: true,
+                                message: '请上传歌曲！'
+                            }
+                        ]
+                    })(<Upload {...props}>
+                        <Button>
+                            <Icon type="upload"/> Click to Upload
+                        </Button>
+                    </Upload>)}
+                </FormItem>
+                <FormItem>
+                <Button type="primary" htmlType="submit">
+                        保存
+                </Button>
+                </FormItem>
+
+            </Form>
         );
     }
 }
 
-export default Songs;
+export default Form.create()(Songs);
